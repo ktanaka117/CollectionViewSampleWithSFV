3e106967cca2de0fc70f64767013be7ac0b165e7
tanaka.kenji <tanaka.kenji@classmethod.jp>
2017-12-21 08:49:13 +0900

    Initial Commit

---
+++ ScrollingCollectionFollowView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollingCollectionFollowView
+//
+//  Created by tanaka.kenji on 2017/12/21.
+//  Copyright © 2017年 tanaka.kenji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
